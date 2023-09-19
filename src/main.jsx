f5a3163a10004be77efdcb242df08fc803c09911
rafiul1000 <rafiulislam5075@gmail.com>
2023-09-19 11:27:26 +0600

    Nested Route and Use Link, outlet

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import {
 import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
+import Users from './components/Users/Users.jsx';
 
 const router = createBrowserRouter([
   {
@@ -21,8 +22,12 @@ const router = createBrowserRouter([
         element: <About></About>
       },
       {
-        path: 'contact',
+        path: '/contact',
         element: <Contact></Contact>
+      },
+      {
+        path: '/users',
+        element: <Users></Users>
       }
     ]
   },
